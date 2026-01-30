356e3743a37397ad914057ccca7e84d081ce5ec8
Emilio J Deltoro <mthornton@fastmail.fm>
2026-01-30 20:07:57 +0000

    added web_sg variable

--- security-groups/sg_var.tf
+++ security-groups/sg_var.tf
@@ -7,9 +7,3 @@ variable "project" {
   description = "Project name - use UserX format where X is your student number"
   type        = string
 }
-Filename3: sg_output.tf
-
-output "web_sg_id" {
-  description = "ID of the web security group"
-  value       = aws_security_group.web_sg.id
-}
